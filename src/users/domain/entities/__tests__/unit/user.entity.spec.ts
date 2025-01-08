9fed6e9a75fe6d3d482ffd8714705655f909809e
matheushmg <matheus.henrique@shiftinc.com.br>
2025-01-08 14:25:01 -0300

    fix: adjusting validation and user

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,14 +1,11 @@
-import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '../../../testing/helpers/user-data-builder';
+
 describe('UserEntity unit tests', () => {
     let props: UserProps;
     let sut: UserEntity;
     beforeEach(() => {
-        props = {
-            name: faker.person.fullName(),
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-        };
+        props = UserDataBuilder({});
         sut = new UserEntity(props);
     });
     it('Constructor method', () => {
@@ -23,10 +20,23 @@ describe('UserEntity unit tests', () => {
         expect(sut.props.name).toEqual(props.name);
         expect(typeof sut.props.name).toBe('string');
     });
+
+    it('Setter of name field', () => {
+        sut['name'] = 'other name';
+        expect(sut.props.name).toEqual('other name');
+        expect(typeof sut.props.name).toBe('string');
+    });
+
     it('Getter of email field', () => {
         expect(sut.props.email).toBeDefined();
         expect(sut.props.email).toEqual(props.email);
         expect(typeof sut.props.email).toBe('string');
+    });
+
+    it('Setter of password field', () => {
+        sut['password'] = 'other password';
+        expect(sut.props.password).toEqual('other password');
+        expect(typeof sut.props.password).toBe('string');
     });
     it('Getter of password field', () => {
         expect(sut.props.password).toBeDefined();
@@ -36,5 +46,14 @@ describe('UserEntity unit tests', () => {
     it('Getter of password field', () => {
         expect(sut.props.createdAt).toBeDefined();
         expect(sut.props.createdAt).toBeInstanceOf(Date);
+    });
+
+    it('Should update a user', () => {
+        sut.update('other name');
+        expect(sut.props.name).toEqual('other name');
+    });
+    it('Should update the password field', () => {
+        sut.updatePassword('other password');
+        expect(sut.props.password).toEqual('other password');
     });
 });
